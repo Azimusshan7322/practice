ce15e29e5cc19779f2444a38363e4e9a4c56bab9
Azimusshan7322 <122336077+Azimusshan7322@users.noreply.github.com>
2023-03-22 23:52:53 +0530

    Add files via upload

---
+++ hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main() {
+    int a,b;
+    printf("enter a :");
+    scanf("%d",&a);
+    printf("enter b:");
+    scanf("%d",&b);
+
+    int power = pow(a,b);
+    printf("answer  is :%d",a%b);
+    return 0;
+}
+
+   
+
